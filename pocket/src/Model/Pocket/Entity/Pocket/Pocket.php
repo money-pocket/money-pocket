bd7835e109c453d1e49af3f31147c6ff430b06ec
Zenkovich <zenkovich@aeroidea.ru>
2020-04-24 18:53:58 +0300

    Network entity init

--- pocket/src/Model/Pocket/Entity/Pocket/Pocket.php
+++ pocket/src/Model/Pocket/Entity/Pocket/Pocket.php
@@ -8,12 +8,21 @@ class Pocket
 {
     private Id $id;
     private ClientId $clientId;
-    private string $network;
-    private string $pocketId;
-    private string $inviteToken;
+    private Network $network;
+    private PocketId $pocketId;
+    private InviteToken $inviteToken;
 
-    public function __construct(Id $id, ClientId $clientId)
-    {
+    public function __construct(
+        Id $id,
+        ClientId $clientId,
+        Network $network,
+        PocketId $pocketId,
+        InviteToken $inviteToken
+    ) {
         $this->id = $id;
+        $this->clientId = $clientId;
+        $this->network = $network;
+        $this->pocketId = $pocketId;
+        $this->inviteToken = $inviteToken;
     }
 }
