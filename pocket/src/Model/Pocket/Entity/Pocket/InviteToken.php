1a5765944ae3400d7d0a045f719ce8bf3c225e99
Zenkovich <zenkovich@aeroidea.ru>
2020-04-24 21:08:09 +0300

    Invite token entity

---
+++ pocket/src/Model/Pocket/Entity/Pocket/InviteToken.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Model\Pocket\Entity\Pocket;
+
+
+class InviteToken
+{
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+}
