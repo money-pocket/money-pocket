ba32442f50be200949cea10c19774f38ec644c5c
zenoleg <zenkovicholeg@gmail.com>
2020-04-26 12:57:01 +0300

    getByClientId method

--- pocket/src/Model/Pocket/Entity/Pocket/PocketRepository.php
+++ pocket/src/Model/Pocket/Entity/Pocket/PocketRepository.php
@@ -4,9 +4,13 @@
 namespace App\Model\Pocket\Entity\Pocket;
 
 
+use App\Model\User\Entity\User\User;
+
 interface PocketRepository
 {
     public function hasByClientId(ClientId $clientId): bool;
+
+    public function getByClientId(ClientId $clientId): User;
 
     public function add(Pocket $pocket): void;
 }
