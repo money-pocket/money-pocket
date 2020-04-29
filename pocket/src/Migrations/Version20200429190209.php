e41e560b28bab5172ee40f9fd27af0ee7d0c8dde
zenoleg <zenkovicholeg@gmail.com>
2020-04-29 22:02:47 +0300

    Remove create schema public

--- pocket/src/Migrations/Version20200429190209.php
+++ pocket/src/Migrations/Version20200429190209.php
@@ -36,7 +36,6 @@ final class Version20200429190209 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE pockets');
     }
 }
