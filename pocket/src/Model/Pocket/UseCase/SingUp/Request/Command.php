5666d7d13d60a20847cbde1ffa0d9c223a36c18f
zenoleg <zenkovicholeg@gmail.com>
2020-04-30 18:52:33 +0300

    Validate SignUp\Request\Command

--- pocket/src/Model/Pocket/UseCase/SingUp/Request/Command.php
+++ pocket/src/Model/Pocket/UseCase/SingUp/Request/Command.php
@@ -3,9 +3,16 @@
 
 namespace App\Model\Pocket\UseCase\SingUp\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
 
 class Command
 {
+    /**
+     * @Assert\NotBlank()
+     */
     public string $clientId;
+    /**
+     * @Assert\NotBlank()
+     */
     public string $network;
 }
