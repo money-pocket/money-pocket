4ea6f84faa645d37cf5c6d2e7de3304e95ba48bd
zenoleg <zenkovicholeg@gmail.com>
2020-04-30 15:27:51 +0300

    Pocket Repository realisation

--- pocket/src/Model/Pocket/Entity/Pocket/PocketRepository.php
+++ pocket/src/Model/Pocket/Entity/Pocket/PocketRepository.php
@@ -4,15 +4,62 @@
 namespace App\Model\Pocket\Entity\Pocket;
 
 
-use App\Model\User\Entity\User\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityNotFoundException;
+use Doctrine\Persistence\ObjectRepository;
 
-interface PocketRepository
+class PocketRepository
 {
-    public function hasByClientId(ClientId $clientId): bool;
+    private EntityManagerInterface $em;
+    private ObjectRepository $repo;
 
-    public function getByClientId(ClientId $clientId): Pocket;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->repo = $em->getRepository(Pocket::class);
+    }
 
-    public function getByInviteToken(string $inviteToken): Pocket;
+    public function hasByClientId(ClientId $clientId): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.client_id = :client_id')
+                ->setParameter(':client_id', $clientId->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
 
-    public function add(Pocket $pocket): void;
+    /**
+     * @param ClientId $clientId
+     * @return Pocket
+     * @throws EntityNotFoundException
+     */
+    public function getByClientId(ClientId $clientId): Pocket
+    {
+        /** @var Pocket $pocket */
+        if (!$pocket = $this->repo->findOneBy(['clientId' => $clientId->getValue()])) {
+            throw new EntityNotFoundException('Pocket is not found.');
+        }
+
+        return $pocket;
+    }
+
+    /**
+     * @param string $inviteToken
+     * @return Pocket
+     * @throws EntityNotFoundException
+     */
+    public function getByInviteToken(string $inviteToken): Pocket
+    {
+        /** @var Pocket $pocket */
+        if (!$pocket = $this->repo->findOneBy(['inviteToken.token' => $inviteToken])) {
+            throw new EntityNotFoundException('Pocket is not found.');
+        }
+
+        return $pocket;
+    }
+
+    public function add(Pocket $pocket): void
+    {
+        $this->em->persist($pocket);
+    }
 }
